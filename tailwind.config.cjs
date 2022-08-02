cfa5a99ed6e1018f48afa504c44c74f67dcdf11f
maurizio.schifano <maurizio.schifano@missing.tech>
2022-08-02 21:10:29 +0200

    json parse read

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}", "./partials/**/*.{hbs,html}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}", "./pages/**/*.{js,ts,jsx,tsx,html}", "./partials/**/*.{hbs,html}"],
   theme: {
     extend: {},
   },
